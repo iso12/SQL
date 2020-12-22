a7114c84fac60f2cae7ec1e2b3d2cf57d73dc410
Ismayil Qarayev <44869102+iso12@users.noreply.github.com>
2020-12-22 01:56:45 -0800

    Update CommandExecute2.sql

--- CommandExecute2.sql
+++ CommandExecute2.sql
@@ -15,4 +15,37 @@ end
 
 if not (select [compatiblity_level] from sys.database where database_id = DB_ID()) >= 90
 BEGIN 
-   SET ErrorMessage = 'database' + 
+   SET ErrorMessage = 'database' + + QUOTENAME(DB_NAME(DB_ID())) + ' has to be in compatibility level 90 or higher.'
+  RAISERROR(@ErrorMessage,16,1) WITH NOWAIT
+END
+
+IF OBJECT_ID('tempdb..#Config') is not null drop table #Config
+create table #Config
+(
+     [Name] nvarchar(max),
+     [Value] nvarchar(max)
+)
+
+insert into #Config ([Name], [Value]) values ('CreateJobs', @CreateJobs)
+insert into #Config ([Name], [Value]) values ('BackupDirectory', @BackupDirectory)
+insert into #Config ([Name], [Value]) values ('CleanupTime', @CleanupTime)
+insert into #Config ([Name], [Value]) values ('OutputFileDirectory', @OutputFileDirectory)
+insert into #Config ([Name], [Value]) values ('LogToTable', @LogToTable)
+insert into  #Config ([Name], [Value]) VALUES('DatabaseName', DB_NAME(DB_ID()))
+GO
+
+set ansi_nulls on
+GO
+set quoted_identifier on
+GO
+if not exists 
+(
+	select * from sys.objects 
+	where OBJECT_ID('N[dbo].[CommandLog]') 
+	and type in (N'U')
+)
+BEGIN 
+create table [dbo].[CommandLog]
+(
+	[ID] [int] 
+)
